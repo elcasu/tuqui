40e022cd691d2177f2e6277791f4ca3ce154bb23
Gabriel Casullo <info@casu-net.com.ar>
2017-11-17 17:43:03 -0300

    testing with screen dimensions

--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,7 @@
 const WIDTH = 2000
 const HEIGHT = 2000
-const WINDOW_WIDTH = window.outerWidth
-const WINDOW_HEIGHT = window.innerHeight
+const WINDOW_WIDTH = window.outerWidth * window.devicePixelRatio
+const WINDOW_HEIGHT = window.innerHeight * window.devicePixelRatio
 const GRAVITY = 200
 
 window.onload = function () {
