ea754e6d963d9eef26476bf622da78c016302ef9
Gabriel Casullo <info@casu-net.com.ar>
2017-11-17 06:44:59 -0300

    change 'AUTO' for 'CANVAS' -> test for mobile

--- public/src/index.js
+++ public/src/index.js
@@ -5,7 +5,7 @@ const WINDOW_HEIGHT = window.innerHeight
 const GRAVITY = 200
 
 window.onload = function () {
-  let game = new Phaser.Game(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO, '')
+  let game = new Phaser.Game(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.CANVAS, '')
   game.state.add('play', PlayState)
   game.state.add('load', LoadingState)
   game.state.start('load')
